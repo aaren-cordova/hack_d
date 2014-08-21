94759681fe14373e64d450f0d973e1eb60a0c47f
Aaren Joseph Cordova <aaren.cordova@demandmedia.com>
2014-08-21 14:31:28 -0700

    Updated wishlist model interface

--- js/s6/widgets/IWishlistModel.js
+++ js/s6/widgets/IWishlistModel.js
@@ -8,7 +8,7 @@ goog.scope(function(){
 	 * @interface
 	 * @extends {goog.events.Listenable}
 	 */
-	s6.widgets.IWishlistModel = function(){;}
+	s6.widgets.IWishlistModel = function(){};
 
 	/** 
 	 * @param {string} name
@@ -24,11 +24,70 @@ goog.scope(function(){
 	 */
 	s6.widgets.IWishlistModel.prototype.setProperty = function(name, value){};
 
+	/** @return {number} */
 	s6.widgets.IWishlistModel.prototype.getWishlistState = function(){};
 
 	/**
-	* @param {number} mode
-	* @return {s6.widgets.IWishlistModel} [description]
+	 * @param {number} state
+	 * @return {number}
+	 */
+	s6.widgets.IWishlistModel.prototype.getIndexOfWishlistState = function(state){};
+
+	/**
+	 * @param {number} state
+	 * @return {s6.widgets.IWishlistModel} [description]
+	 */
+	s6.widgets.IWishlistModel.prototype.setWishlistState = function(state){};
+
+
+	/** @return {Node} */
+	s6.widgets.IWishlistModel.prototype.getNode = function(){};
+
+	/** 
+	 * @param {Node} wishlistModelNode
+	 * @return {s6.widgets.IWishlistModel}
+	 */
+	s6.widgets.IWishlistModel.prototype.setNode = function(wishlistModelNode){};
+
+	/** 
+	* @return {number}
 	*/
-	s6.widgets.IWishlistModel.prototype.setWishlistState = function(mode){};
+	s6.widgets.IWishlistModel.prototype.getNumItems = function(){};
+
+	/** 
+	* @param {number} index
+	* @return {s6.widgets.IArtPieceModel}
+	*/
+	s6.widgets.IWishlistModel.prototype.getItemAt = function(index){};
+
+	/**
+	 * @param {!s6.widgets.IArtPieceModel} item
+	 * @return {s6.widgets.IWishlistModel}
+	 */
+	s6.widgets.IWishlistModel.prototype.addItem = function(item){};
+
+	/** 
+	 * @param {!s6.widgets.IArtPieceModel} item
+	 * @param {number} index
+	 * @return {s6.widgets.IWishlistModel}
+	 */
+	s6.widgets.IWishlistModel.prototype.addItemAt = function(item, index){};
+
+	/**
+	 * @param {!s6.widgets.IArtPieceModel} item
+	 * @return {s6.widgets.IWishlistModel}
+	 */
+	s6.widgets.IWishlistModel.prototype.removeItem = function(item){};
+
+	/**
+	 * @param {number} index
+	 * @return {number}
+	 */
+	s6.widgets.IWishlistModel.prototype.removeItemAt = function(index){};
+
+	/** 
+	* @param {s6.widgets.IArtPieceModel}
+	* @return number
+	*/
+	s6.widgets.IWishlistModel.prototype.indexOfItem = function(item){};
 });
